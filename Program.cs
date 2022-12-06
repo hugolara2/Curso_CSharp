8d7f923ec11410a711f1304cac1015465bd9185d
Hugo Lara <hugo.alex200197@outlook.com>
2022-12-06 17:53:15 -0600

    Beep removed

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ namespace CoreEscuela
             var engine = new EscuelaEngine();
             engine.Inicializar();
             Printer.WriteTitle("BIENVENIDOS A LA ESCUELA");
-            Printer.Beep(10000, cantidad:10);
             ImpimirCursosEscuela(engine.Escuela);
         }
 
